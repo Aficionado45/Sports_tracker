866df44374202ae80e48f41b770163232ceec536
Aficionado45 <varenya.tiwari@iitg.ac.in>
2021-05-06 21:38:47 +0530

    Added themes.dart to master

---
+++ lib/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color primaryBlue = Color.fromRGBO(34, 44, 72, 1);
+Color secondaryGreen = Color.fromRGBO(72, 190, 154, 1);
+TextStyle heading = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
+TextStyle heading1 =
+    TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white);
+TextStyle subheading = TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+TextStyle subheading1 =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white);
+TextStyle normal = TextStyle(fontSize: 16, fontWeight: FontWeight.w300);
+TextStyle normal1 =
+    TextStyle(fontSize: 16, fontWeight: FontWeight.w300, color: Colors.white);
