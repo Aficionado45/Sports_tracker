201705db4f5d154d5dbfb455ebf874529f1df151
Aficionado45 <varenya.tiwari@iitg.ac.in>
2021-05-28 17:29:03 +0530

    About to be Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,31 @@
-// ignore: unused_import
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sports_web/home_screen.dart';
+import 'package:sports_web/reset.dart';
+import 'login_screen.dart';
+import 'leaderboard.dart';
+import 'reset.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fitness Tracker'),
-        ),
-        body: Center(
-          child: Container(child: Text('Login Screen')),
-        ),
-      ),
+      initialRoute: 'login',
+      routes: {
+        //Add all the page navigation refrences here to navigate across screens.
+        'login': (context) => Login(),
+        'home': (context) => Home(),
+        'leader': (context) => Leaderboard(),
+        'reset': (context) => ResetScreen(),
+        //'admin:(context) => AdminScreen(),
+      },
     );
   }
 }
