24e31021a9fabe3b427cde320ab504d9114bbdc9
Aficionado45 <varenya.tiwari@iitg.ac.in>
2021-05-29 14:31:22 +0530

    Debug

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -31,178 +31,185 @@ class _LoginState extends State<Login> {
               end: FractionalOffset.bottomCenter,
               tileMode: TileMode.repeated)),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          children: [
-            Row(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Column(
               children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                      height: 80,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'login');
+                      },
+                      child: const Text("LOGIN"),
+                      style: TextButton.styleFrom(
+                          primary: Color(0XFFD3C48D),
+                          textStyle: TextStyle(fontSize: 20)),
+                    ),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {
+                        //Navigator.pushNamed(context,'admin'); // Chnage the navigator path to admin console
+                      },
+                      child: const Text("ADMIN"),
+                      style: TextButton.styleFrom(
+                          primary: Color(0XFFD3C48D),
+                          textStyle: TextStyle(fontSize: 20)),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
                 SizedBox(
-                  width: 20,
-                  height: 80,
+                  height: 150,
+                ),
+                Text(
+                  "Website Name",
+                  style: TextStyle(
+                    color: Color(0xFFFFFAFA),
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
+                  height: 50,
+                  width: MediaQuery.of(context).size.height / 1.8,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.start,
+                    onChanged: (value) {
+                      if (value.isEmpty)
+                        return Null;
+                      else {
+                        email = value;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle:
+                          TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding:
+                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
+                  width: MediaQuery.of(context).size.height / 1.8,
+                  height: 50,
+                  child: TextField(
+                    obscureText: true,
+                    onChanged: (value) {
+                      if (value.isEmpty)
+                        return Null;
+                      else {
+                        password = value;
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.start,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle:
+                          TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding:
+                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
                 ),
                 TextButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, 'login');
+                    Navigator.pushNamed(context, 'reset');
                   },
-                  child: const Text("LOGIN"),
+                  child: const Text("Forgot your password?"),
                   style: TextButton.styleFrom(
-                      primary: Color(0XFFD3C48D),
-                      textStyle: TextStyle(fontSize: 20)),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {
-                    //Navigator.pushNamed(context,'admin'); // Chnage the navigator path to admin console
-                  },
-                  child: const Text("ADMIN"),
-                  style: TextButton.styleFrom(
-                      primary: Color(0XFFD3C48D),
-                      textStyle: TextStyle(fontSize: 20)),
+                      primary: Color(0XFF5DB075),
+                      textStyle: TextStyle(fontSize: 16)),
                 ),
                 SizedBox(
-                  width: 20,
+                  height: 10,
                 ),
+                Container(
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.all(0.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF73DCDC),
+                              Color(0xFFC11ADC),
+                            ],
+                          ),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(100.0))),
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: const Text('Log In',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ),
+                    onPressed: () async {
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        if (user != null) {
+                          Navigator.pushNamed(context, 'home');
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 170,
+                ),
+                Text(
+                  "Sports Board IITG",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Color(0XFFD3C48D),
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Developed By: Varenya Tiwari, Karan Jain, Ayush Raj, Aayush Sachdeva",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      color: Color(0X90D3C48D),
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic),
+                ),
+                SizedBox(height: 10),
               ],
             ),
-            SizedBox(
-              height: 150,
-            ),
-            Text(
-              "Website Name",
-              style: TextStyle(
-                color: Color(0xFFFFFAFA),
-                fontSize: 30,
-              ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
-              height: 50,
-              width: MediaQuery.of(context).size.height / 1.8,
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.start,
-                onChanged: (value) {
-                  if (value.isEmpty)
-                    return Null;
-                  else {
-                    email = value;
-                  }
-                },
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  hintStyle: TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFE8E8E8)),
-                      borderRadius: BorderRadius.circular(10.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
-              width: MediaQuery.of(context).size.height / 1.8,
-              height: 50,
-              child: TextField(
-                obscureText: true,
-                onChanged: (value) {
-                  if (value.isEmpty)
-                    return Null;
-                  else {
-                    password = value;
-                  }
-                },
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFE8E8E8)),
-                      borderRadius: BorderRadius.circular(10.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, 'reset');
-              },
-              child: const Text("Forgot your password?"),
-              style: TextButton.styleFrom(
-                  primary: Color(0XFF5DB075),
-                  textStyle: TextStyle(fontSize: 16)),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: RaisedButton(
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0)),
-                child: Container(
-                  decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: <Color>[
-                          Color(0xFF73DCDC),
-                          Color(0xFFC11ADC),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(100.0))),
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: const Text('Log In',
-                      style: TextStyle(color: Colors.white, fontSize: 16)),
-                ),
-                onPressed: () async {
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
-                    if (user != null) {
-                      Navigator.pushNamed(context, 'home');
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-            ),
-            SizedBox(
-              height: 170,
-            ),
-            Text(
-              "Sports Board IITG",
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color: Color(0XFFD3C48D),
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Developed By: Varenya Tiwari, Karan Jain, Ayush Raj, Aayush Sachdeva",
-              style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  color: Color(0X90D3C48D),
-                  fontSize: 12,
-                  fontStyle: FontStyle.italic),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
--- lib/reset.dart
+++ lib/reset.dart
@@ -26,71 +26,74 @@ class _ResetScreenState extends State<ResetScreen> {
                 end: FractionalOffset.bottomCenter,
                 tileMode: TileMode.repeated)),
         child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
-            children: [
-              SizedBox(
-                height: 200,
-              ),
-              Container(
-                //padding: EdgeInsets.only(left: 150, right: 150, bottom: 30),
-                height: 50,
-                width: MediaQuery.of(context).size.height / 1.8,
-                child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    textAlign: TextAlign.start,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle:
-                          TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFE8E8E8)),
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        _email = value.trim();
-                      });
-                    }),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+            backgroundColor: Colors.transparent,
+            body: SingleChildScrollView(
+              child: Column(
                 children: [
-                  RaisedButton(
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0)),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Color(0xFF73DCDC),
-                              Color(0xFFC11ADC),
-                            ],
-                          ),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(100.0))),
-                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      child: const Text('Send Request',
-                          style: TextStyle(color: Colors.white, fontSize: 16)),
-                    ),
-                    onPressed: () {
-                      _auth.sendPasswordResetEmail(email: _email);
-                      Navigator.of(context).pop();
-                    },
+                  SizedBox(
+                    height: 200,
+                  ),
+                  Container(
+                    //padding: EdgeInsets.only(left: 150, right: 150, bottom: 30),
+                    height: 50,
+                    width: MediaQuery.of(context).size.height / 1.8,
+                    child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle:
+                              TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _email = value.trim();
+                          });
+                        }),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      RaisedButton(
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80.0)),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: <Color>[
+                                  Color(0xFF73DCDC),
+                                  Color(0xFFC11ADC),
+                                ],
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100.0))),
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: const Text('Send Request',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                        ),
+                        onPressed: () {
+                          _auth.sendPasswordResetEmail(email: _email);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      SizedBox(height: 30),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ));
+            )));
   }
 }
