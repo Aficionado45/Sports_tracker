4ce6c30f054b63a28e4dba3f76d516f818de59dd
ayush <ayushrajsharma8051@gmail.com>
2021-06-04 04:50:38 +0530

    uploaded admin screen some task in video list left

---
+++ lib/adminLogin.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sports_web/ClubListScreen.dart';
+
+
+class AdminLogin extends StatefulWidget {
+  const AdminLogin({Key key}) : super(key: key);
+
+  @override
+  _AdminLoginState createState() => _AdminLoginState();
+}
+
+class _AdminLoginState extends State<AdminLogin> {
+  @override
+  final _auth = FirebaseAuth.instance;
+  String email, password;
+
+  void initState() {
+    super.initState();
+  }
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('sports_ball.jpg'), fit: BoxFit.fill),
+          ),
+        ),
+        Container(
+          decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                  colors: [
+                    Color.fromRGBO(70, 75, 75, 88),
+                    Color.fromRGBO(0, 0, 0, 100),
+                  ],
+                  stops: [
+                    0.0,
+                    1.0
+                  ],
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  tileMode: TileMode.repeated)),
+          child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 80,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'login');
+                          },
+                          child: const Text("LOGIN"),
+                          style: TextButton.styleFrom(
+                              primary: Color(0XFFD3C48D),
+                              textStyle: TextStyle(fontSize: 20)),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {
+                            //Navigator.pushNamed(context,'admin'); // Chnage the navigator path to admin console
+                          },
+                          child: const Text("ADMIN"),
+                          style: TextButton.styleFrom(
+                              primary: Color(0XFFD3C48D),
+                              textStyle: TextStyle(fontSize: 20)),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Text(
+                      "Website Name",
+                      style: TextStyle(
+                        color: Color(0xFFFFFAFA),
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
+                      height: 50,
+                      width: MediaQuery.of(context).size.height / 1.8,
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.start,
+                        onChanged: (value) {
+                          if (value.isEmpty)
+                            return Null;
+                          else {
+                            email = value;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle:
+                          TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding:
+                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
+                      width: MediaQuery.of(context).size.height / 1.8,
+                      height: 50,
+                      child: TextField(
+                        obscureText: true,
+                        onChanged: (value) {
+                          if (value.isEmpty)
+                            return Null;
+                          else {
+                            password = value;
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle:
+                          TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding:
+                          EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'reset');
+                      },
+                      child: const Text("Forgot your password?"),
+                      style: TextButton.styleFrom(
+                          primary: Color(0XFF5DB075),
+                          textStyle: TextStyle(fontSize: 16)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80.0)),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: <Color>[
+                                  Color(0xFF73DCDC),
+                                  Color(0xFFC11ADC),
+                                ],
+                              ),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(100.0))),
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: const Text('Log In',
+                              style:
+                              TextStyle(color: Colors.white, fontSize: 16)),
+                        ),
+                        onPressed: () async {
+                          try {
+                            final user = await _auth.signInWithEmailAndPassword(
+                                email: email, password: password);
+                            if (user != null) {
+                              print(user.toString());
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>ClubListScreen()));
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 170,
+                    ),
+                    Text(
+                      "Sports Board IITG",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFFD3C48D),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Developed By: Varenya Tiwari, Karan Jain, Ayush Raj, Aayush Sachdeva",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          color: Color(0X90D3C48D),
+                          fontSize: 12,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              )),
+        )
+      ],
+    );
+  }
+}
+
---
+++ lib/auth_screen.dart
@@ -0,0 +1,207 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sports_web/adminLogin.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  final _auth = FirebaseAuth.instance;
+  String _keypassword;
+  String pass;
+  void getauthenticated() async{
+    DocumentSnapshot ds = await FirebaseFirestore.instance.collection("authenticationKey").doc('admin').get();
+    setState(() {
+      pass = ds.get('admin');
+    });
+  }
+
+  void initState() {
+    getauthenticated();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('sports_ball.jpg'), fit: BoxFit.fill),
+          ),
+        ),
+        Container(
+          decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                  colors: [
+                Color.fromRGBO(70, 75, 75, 88),
+                Color.fromRGBO(0, 0, 0, 100),
+              ],
+                  stops: [
+                0.0,
+                1.0
+              ],
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  tileMode: TileMode.repeated)),
+          child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 80,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'login');
+                          },
+                          child: const Text("LOGIN"),
+                          style: TextButton.styleFrom(
+                              primary: Color(0XFFD3C48D),
+                              textStyle: TextStyle(fontSize: 20)),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {
+                            //Navigator.pushNamed(context,'admin'); // Chnage the navigator path to admin console
+                          },
+                          child: const Text("ADMIN"),
+                          style: TextButton.styleFrom(
+                              primary: Color(0XFFD3C48D),
+                              textStyle: TextStyle(fontSize: 20)),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Text(
+                      "Website Name",
+                      style: TextStyle(
+                        color: Color(0xFFFFFAFA),
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      //margin: EdgeInsets.fromLTRB(150, 0, 150, 0),
+                      height: 50,
+                      width: MediaQuery.of(context).size.height / 1.8,
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        textAlign: TextAlign.start,
+                        onChanged: (value) {
+                          if (value.isEmpty)
+                            return Null;
+                          else {
+                            _keypassword = value;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter key',
+                          hintStyle:
+                              TextStyle(color: Color(0XFFBDBDBD), fontSize: 16),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFE8E8E8)),
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    const Text("Entry key for Admin or Secy portal", style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white
+                    ),),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80.0)),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: <Color>[
+                                  Color(0xFF73DCDC),
+                                  Color(0xFFC11ADC),
+                                ],
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100.0))),
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: const Text('Next',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                        ),
+                        onPressed: () async {
+                          try {
+                            if(_keypassword == pass){
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>AdminLogin()));
+                            }
+                            // if(_keypassword == "seCy"){
+                            //   Navigator.pushNamed(context, 'secy');
+                            // }
+
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 170,
+                    ),
+                    Text(
+                      "Sports Board IITG",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFFD3C48D),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Developed By: Varenya Tiwari, Karan Jain, Ayush Raj, Aayush Sachdeva",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          color: Color(0X90D3C48D),
+                          fontSize: 12,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              )),
+        )
+      ],
+    );
+  }
+  
+}
