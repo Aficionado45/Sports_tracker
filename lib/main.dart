d3a0d8108c102be9a1bdab601a662130e200ce7e
asachdeva69 <davidvillaaayush@gmail.com>
2021-07-17 17:28:55 +0530

    Test Input

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         title: "IIT Fitness Tracker",
-       initialRoute: isLoggedIn? isUserAdmin? 'clubList':'home': 'login',
+       initialRoute: isLoggedIn? isUserAdmin? 'clubList':'home': 'admin',
       routes: {
         //Add all the page navigation refrences here to navigate across screens.
         'login': (context) => Login(),
